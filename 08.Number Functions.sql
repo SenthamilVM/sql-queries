2fe7cd472cb45f05662b75aa5c9989679deccd16
Senthamil <senthamilvm@gmail.com>
2026-01-26 02:10:50 +0530

    Number Functions

---
+++ 08.Number Functions.sql
@@ -0,0 +1,13 @@
+--Number Functions
+--ROUND
+SELECT
+3.516,
+ROUND(3.516,2) AS round_2,
+ROUND(3.516,1) AS round_1,
+ROUND(3.516,0) AS round_0
+
+--ABS -- returns the absolute(positive)value of a number, removing any negative sign
+SELECT
+-10,
+ABS(-10),
+ABS(10)
